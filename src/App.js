8638f88e9d39739529a78ea629104166d2745673
Gavin <gavinlyons87@gmail.com>
2020-10-12 22:54:03 +0100

    Rotate Angle UI components

--- src/App.js
+++ src/App.js
@@ -2,21 +2,51 @@ import React, {useState} from 'react';
 
 import DisplayImage from './Components/DisplayImage/DisplayImage';
 import ImageSelection from './Components/ImageSelection/ImageSelection';
+import Rotation from './Components/Rotation/Rotation';
 
 import './App.css';
 
 function App() {
-  const [selecetedImage, setSelectedImage] = useState(null);
+  const [selecetedImage, setSelectedImage] = useState(null),
+    [rotatedImage, setRotatedImage] = useState(null),
+    [rotationTime, setRotationTime]= useState(0);
 
   const uploadedImage = (imageData) => {
     setSelectedImage(imageData);
+    };
+
+  const rotate = (angle) => {
+    if(!uploadedImage){
+      return alert('Please upload an image first');
+    }
+    
+    try {
+      const radians = parseFloat((angle * (Math.PI / 180).toFixed(2)));
+    
+      const performanceStart = performance.now(),
+        rotateFunction = new rotateFunction(),
+        result = rotateFunction.rotate(uploadedImage, radians),
+        performanceEnd = performance.now();
+    
+      setRotatedImage(result);
+    
+      imageRotationTime(performanceEnd - performanceStart);
+    } catch(error) {
+      alert(error.message);
+    }
+    }
+    
+    const imageRotationTime = (time) => {
+    setRotationTime(parseInt(time));
     };
 
   return (
     <div className="App">
       <h1>Image Rotate UI</h1>
       <ImageSelection uploadedImage={uploadedImage} />
+      <Rotation rotate={rotate}></Rotation>
       <DisplayImage title="Uploaded Image" displayImage={selecetedImage} />
+      <DisplayImage title="Rotated Image" subtitle={rotationTime > 0 ? '(Completion Time $(rotationTime) ms)' : ""} displayImage={rotatedImage} />  
     </div>
   );
 }
--- src/Components/Rotation/Rotation.js
+++ src/Components/Rotation/Rotation.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './Rotation.css';
 
-function Rotation(){
+function Rotation({rotate}){
+    const [angle, setAngle] = useState(0.0);
+
+    const rotateImage = () => {
+        rotate(angle);
+    }
+
     return (
-        <div>
-            Rotation
+        <div className="RotationContainer">
+            <div className="RotationAngle">
+                <label>Angle: </label>
+                <input type="number" name="angle" step="1" defaultValue="0" onChange={e => setAngle(e.target.value)} />
+                <label> degrees</label>
+            </div>
+            <div className="RotationButton">
+                <button onClick={rotateImage}>Rotate</button>
+            </div>
         </div>
 
     )
